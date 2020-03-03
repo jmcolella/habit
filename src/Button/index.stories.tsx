200d43c355735c8c1948086cc4cff1cbcb1eb2c1
John Colella <colella.john@gmail.com>
2020-03-03 09:11:15 -0500

    Storybook initial setup
    Get Storybook development up and running with a simple Button component.
    Focus here is to get moving with development. Future iterations for
    production deployment will be considered when this project takes a more
    clear form.

---
+++ src/Button/index.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from './button';
+
+export default { component: Button, title: 'Button' }
+
+export const Hello = () => (
+  <Button onClick={() => alert('hello!')}>Hello!</Button>
+);
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-function multiply(x: number, y: number): number {
-  return x * y;
-}
-
-console.log(multiply(2, 4));
