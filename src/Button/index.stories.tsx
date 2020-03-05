848595d4a93f91c4202c4d476f052c4a9294d2e8
John Colella <colella.john@gmail.com>
2020-03-04 21:39:47 -0500

    Add eslint configuration
    To ensure that this repo has consistent code quality there should be a
    linter present. This will use ESLint with a configuration setup by
    Policygenius.

--- src/Button/index.stories.tsx
+++ src/Button/index.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './button';
 
-export default { component: Button, title: 'Button' }
+export default { component: Button, title: 'Button' };
 
 export const Hello = () => (
   <Button onClick={() => alert('hello!')}>Hello!</Button>
