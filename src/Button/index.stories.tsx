01387299c2045b5286fe5d99eba69c2bec487cda
John Colella <colella.john@gmail.com>
2020-03-05 08:56:26 -0500

    Add styled-components
    In order to have styling for this component library, we need CSS! This
    library has opted to use the styled-components library because 1) it is
    an increasingly popular styling library for React components and 2) it
    allows for pure CSS based on props.

--- src/Button/index.stories.tsx
+++ src/Button/index.stories.tsx
@@ -6,3 +6,7 @@ export default { component: Button, title: 'Button' };
 export const Hello = () => (
   <Button onClick={() => alert('hello!')}>Hello!</Button>
 );
+
+export const Disabled = () => (
+  <Button onClick={() => alert('hello!')} disabled>Disabled!</Button>
+);
